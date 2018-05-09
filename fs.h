d710186152d497564e3d1b598488fa7a00972f12
Jiaping_Wang <nflshc2020@gmail.com>
2018-05-09 19:37:32 -0400

    syntax fixed

--- fs.h
+++ fs.h
@@ -24,6 +24,8 @@ int traverse_dir(int dirinode_index, string filename);
 int get_next_free_block(int block_index);
 vector<string> split(const string &s, char delim);
 size_t get_file_size(string file_name);
+int change_mode(int mode, string path);
+char* pwd(string file_path);
 #endif
 
 
