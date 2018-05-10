59d13af606376e5b500d56a8d79161a84f142e03
Jiaping_Wang <nflshc2020@gmail.com>
2018-05-10 00:27:46 -0400

    change mode close

--- fs.cpp
+++ fs.cpp
@@ -2941,7 +2941,7 @@ int change_mode(int mode, string path) {
 		this_inode->permission = mode;
 		lseek(disk, BOOT_SIZE + SUPER_SIZE + sb->inode_offset * BLOCK_SIZE + inode_index * sizeof(inode), SEEK_SET);
 		write(disk, this_inode, sizeof(inode));
-		f_close(fd);ch
+		f_close(fd);
 		return SUCCESS;
 	}
 }
